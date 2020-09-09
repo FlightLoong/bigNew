bdfbadf249d046c54f61cf21cc809087da94a303
FlightLoong <1577886278@qq.com>
2020-09-09 15:47:27 +0800

    登录和注册布局完成

---
+++ assets/js/login.js
@@ -0,0 +1,13 @@
+$(function () {
+  // 点击登录按钮
+  $('#link_reg').on('click', function () {
+    $('.login-box').hide()
+    $('.reg-box').show()
+  })
+
+  // 点击注册按钮
+  $('#link_login').on('click', function () {
+    $('.login-box').show()
+    $('.reg-box').hide()
+  })
+})
