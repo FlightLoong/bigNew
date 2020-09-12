c147bea32cec93863832ed750a094e3948f06268
FlightLoong <1577886278@qq.com>
2020-09-12 09:59:33 +0800

    后台主页

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -1,7 +1,22 @@
-// 这个方法用于在 发送 ajax 之前进行配置相关的属性
-// 当发送 $.ajax 等等方法的时候，都会先触发这个方法
+// 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
+// 会先调用 ajaxPrefilter 这个函数
+// 在这个函数中，可以拿到我们给Ajax提供的配置对象
+$.ajaxPrefilter(function (options) {
+  // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
+  options.url = 'http://ajax.frontend.itheima.net' + options.url
 
-// option 是指 $.ajax 请求所有的参数配置
-$.ajaxPrefilter(function (option) {
-  option.url = 'http://ajax.frontend.itheima.net' + option.url
+  // 统一为有权限对的接口设置 headers 请求头
+  if (options.url.indexOf('/my') !== -1) {
+    options.headers = {
+      Authorization: localStorage.getItem('token') || ''
+    }
+  }
+
+  options.complete = function (res) {
+    console.log(res)
+    if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+      localStorage.removeItem('token')
+      location.href = '/login.html'
+    }
+  }
 })
